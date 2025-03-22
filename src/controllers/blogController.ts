158d08d760fd6fbd47b56f4bdae6f1df5c590a16
Dzemil Sahovic <dzemil.sahovic@gmail.com>
2025-03-22 23:08:23 +0100

    Added a blog controller

---
+++ src/controllers/blogController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express';
+import BlogService from '../services/blogService';
+
+const blogService = new BlogService();
+
+class BlogController {
+  async createBlog(req: Request, res: Response) {
+    const data = req.body;
+    const blog = await blogService.createBlog(data);
+    res.status(201).json(blog);
+  }
+
+  async getBlogs(req: Request, res: Response) {
+    const blogs = await blogService.getBlogs();
+    res.json(blogs);
+  }
+
+  async getBlog(req: Request, res: Response) {
+    const id = parseInt(req.params.id);
+    const blog = await blogService.getBlog(id);
+    res.json(blog);
+  }
+
+  async updateBlog(req: Request, res: Response) {
+    const id = parseInt(req.params.id);
+    const data = req.body;
+    const blog = await blogService.updateBlog(id, data);
+    res.json(blog);
+  }
+
+  async deleteBlog(req: Request, res: Response) {
+    const id = parseInt(req.params.id);
+    await blogService.deleteBlog(id);
+    res.json({ message: 'Blog deleted successfully' });
+  }
+}
+
+export default BlogController;
