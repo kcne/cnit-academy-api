d3762a2a76af833c9536c985beb64fffd0306d89
Dzemil Sahovic <dzemil.sahovic@gmail.com>
2025-03-23 23:33:52 +0100

    Added route for publishing

--- src/routes/blogRoutes.ts
+++ src/routes/blogRoutes.ts
@@ -4,10 +4,16 @@ import BlogController from "../controllers/blogController";
 const router = express.Router();
 const blogController = new BlogController();
 
-router.post("/blogs", blogController.createBlog);
+// GET routes
+
 router.get("/blogs", blogController.getBlogs);
 router.get("/blogs/:id", blogController.getBlog);
+
+// POST, PUT, DELETE routes
+
+router.post("/blogs", blogController.createBlog);
 router.put("/blogs/:id", blogController.updateBlog);
+router.put("/blogs/:id/publish", blogController.togglePublishBlog);
 router.delete("/blogs/:id", blogController.deleteBlog);
 
 export default router;
