24b96e3da174a46bc66e0d7a0cd1e257430eb873
segatcm <segatheycallme@proton.me>
2025-03-26 20:16:21 +0100

    resolve conflicts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import userRoutes from "./routes/userRoutes";
 import authRoutes from "./routes/authRoutes";
+import courseRoutes from "./routes/courseRoutes";
 import profileRoutes from "./routes/profileRoutes";
-import leaderboardRoutes from "./routes/leaderboardRoutes";
 import cors from "cors";
+import programRoutes from "./routes/programRoutes";
+import leaderboardRoutes from "./routes/leaderboardRoutes";
 
 const app = express();
 
@@ -11,8 +13,10 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api", authRoutes);
+app.use("/api", courseRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/profile", profileRoutes);
+app.use("/api/programs", programRoutes);
 app.use("/api/leaderboard", leaderboardRoutes);
 
 app.use((_, res) => {
